6f2b6f0a16661e718dce39d2da4c6efd53622b2c
Samandarrrr <saman9779dar@gmail.com>
2023-01-01 16:39:46 +0500

    project Finished

--- README.md
+++ README.md
@@ -5,3 +5,20 @@ a task
 <p align="center">
   <img src="C:\Users\ssaMann\Pictures\Saved Pictures\arxitec.png" width="350" >
 </p>
+
+# use
+To Start it
+```
+php index
+```
+
+Time must be specified in this format
+```
+yyyy-mm-dd hh:mm:ss
+```
+
+To send an email notification
+```
+php send-notification
+```
+
--- src/DB/ConnectToDb.php
+++ src/DB/ConnectToDb.php
@@ -5,23 +5,54 @@ namespace Src\DB;
 class  ConnectToDb
 {
 
+    /**
+     * @var string
+     */
     public $host = 'localhost'; // имя хоста
 
+    /**
+     * @var string
+     */
     public $user = 'root';      // имя пользователя
 
+    /**
+     * @var string
+     */
     public $pass = '';          // пароль
 
+    /**
+     * @var string
+     */
     public $name = "reception";   // имя базы данных
 
+    /**
+     * @var
+     */
     public $busy_info;
 
+    /**
+     * @var
+     */
     public $empty_info;
 
+    /**
+     *
+     * Function  connect
+     * @return  false|\mysqli
+     */
     public function connect()
     {
         return mysqli_connect($this->host, $this->user, $this->pass, $this->name);
     }
 
+    /**
+     *
+     * Function  isCabinetEmpty
+     * @param $cabinet_number
+     * @param $start
+     * @param $end
+     * @return  bool|string|null
+     */
     public function isCabinetEmpty($cabinet_number, $start, $end)
     {
         $link = $this->connect();
@@ -29,51 +60,75 @@ class  ConnectToDb
         $result = mysqli_query($link, $query);
         $result_assoc = mysqli_fetch_all($result, MYSQLI_ASSOC);
         $link->close();
-
-
         $counter = 0;
+        if ($result->num_rows) {
+            foreach ($result_assoc as $item) {
 
-        foreach ($result_assoc as $item) {
-
-            $res = $this->isBetweenEmpty($item['start_date'], $item['end_date'], $start, $end);
-
-            if ($res === 'Ты тупой или Тестер') {
-                $type = $res;
-                break;
-
-            } else if ($res !== 'Ты тупой или Тестер' && !$res) {
-                $this->busy_info = $type = $this->ifBusy($item['user_id'], $item['start_date'], $item['end_date']);
-                break;
-
-            } else if ($res && !$this->busy_info) {
-
-                if ($this->empty_info) {
+                $res = $this->isBetweenEmpty($item['start_date'], $item['end_date'], $start, $end);
+                if ($res === 'Ты тупой или Тестер') {
+                    $type = $res;
                     break;
-                } else if ($counter === count($result_assoc) - 1) {
-                    $this->empty_info = $type = $this->ifEmpty($cabinet_number);
-                }
-            } else {
-                echo $res;
-            }
 
-            $counter = $counter + 1;
+                } else if ($res !== 'Ты тупой или Тестер' && !$res) {
+                    $this->busy_info = $type = $this->ifBusy($item['user_id'], $item['start_date'], $item['end_date']);
+                    break;
+
+                } else if (($res && !$this->busy_info)) {
+
+                    if ($this->empty_info) {
+                        break;
+                    } else if ($counter === count($result_assoc) - 1) {
+                        $this->empty_info = $type = $this->ifEmpty($cabinet_number, $start, $end);
+                    }
+                } else {
+                    $type = $res;
+                    break;
+                }
+
+                $counter = $counter + 1;
+            }
+        } else {
+            $type = $this->ifEmpty($cabinet_number, $start, $end);
         }
 
 
         return $type;
     }
 
+    /**
+     *
+     * Function  IsBetweenEmpty
+     * @param $db_start
+     * @param $db_end
+     * @param $user_start
+     * @param $user_end
+     * @return  bool|string
+     */
     public function IsBetweenEmpty($db_start, $db_end, $user_start, $user_end)
     {
+
         if (strtotime($user_start) <= strtotime($user_end)) {
-
-            if (((strtotime($user_start) >= strtotime($db_start)) and (strtotime($user_start) >= strtotime($db_end))) or ((strtotime($user_start) <= strtotime($db_start)) and (strtotime($user_start) <= strtotime($db_end)))) {
-
-                return true;
-
-            } else {
+            if ((strtotime($user_start) <= strtotime($db_start)) && (strtotime($user_end) <= strtotime($db_end))) {
 
                 return false;
+
+            } else if ((strtotime($user_start) >= strtotime($db_start)) && (strtotime($user_start) <= strtotime($db_end))) {
+
+                return false;
+
+            } else if ((strtotime($user_start) <= strtotime($db_end)) && (strtotime($user_start) >= strtotime($db_end))) {
+
+                return false;
+
+            }
+            else if ((strtotime($user_start) <= strtotime($db_end)) && (strtotime($user_end) >= strtotime($db_end))){
+
+                return false;
+
+            }
+            else {
+
+                return true;
 
             }
         } else {
@@ -81,7 +136,16 @@ class  ConnectToDb
         }
     }
 
-    public  function ifBusy($user_id, $start, $end){
+    /**
+     *
+     * Function  ifBusy
+     * @param $user_id
+     * @param $start
+     * @param $end
+     * @return  string
+     */
+    public function ifBusy($user_id, $start, $end)
+    {
         $link = $this->connect();
         $query = 'SELECT name FROM USERS WHERE ID = ' . $user_id;
         $result = mysqli_query($link, $query);
@@ -90,31 +154,62 @@ class  ConnectToDb
         return "This Room is busy from " . $result_assoc[0]['name'] . '  from:' . $start . ' to: ' . $end;
     }
 
-    public function ifEmpty($cabinet_number){
+    /**
+     *
+     * Function  ifEmpty
+     * @param $cabinet_number
+     */
+    public function ifEmpty($cabinet_number, $start, $end)
+    {
         $name = readline('Enter your name: ');
         $email = readline('Enter your email: ');
         $phone = readline('Enter your phone number: ');
-        
-        $this->saveUser($name, $email, $phone);
-        die();
-        $this->saveOrder($cabinet_number, $name, $email, $phone);
-        return "This room is free: \n";
+
+        (int)$id = $this->saveUser($name, $email, $phone);
+        $this->saveOrder($cabinet_number, $id, $start, $end);
+        return "\nOrder received\n";
     }
 
-    public function saveUser($name, $email, $phone){
+    /**
+     *
+     * Function  saveUser
+     * @param $name
+     * @param $email
+     * @param $phone
+     */
+    public function saveUser($name, $email, $phone)
+    {
+        $link = $this->connect();
+
+
+        $query = "INSERT INTO users (name, email, phone_number) VALUES ('$name', '$email' , '$phone')";
+        mysqli_query($link, $query);
+
+        return mysqli_insert_id($link);
+
+    }
+
+    /**
+     *
+     * Function  saveOrder
+     * @param $cabinet_number
+     * @param $user_id
+     * @param $star_date
+     * @param $end_date
+     */
+    public function saveOrder($cabinet_number, $user_id, $start_date, $end_date)
+    {
         $link = $this->connect();
         try {
 
-            $query = "INSERT INTO users (name, email, phone) VALUES ('.$name .', ' .$email .'  , '.$phone.')";
+            $query = "INSERT INTO orders (cabinet_number, user_id, start_date, end_date, notification) VALUES ('$cabinet_number', '$user_id', '$start_date', '$end_date', 0)";
+
             mysqli_query($link, $query);
-        }catch(\Exception $e) {
+
+            return mysqli_insert_id($link);
+
+        } catch (\Exception $e) {
             echo $e->getMessage();
         }
-    }
-
-    public function saveOrder($cabinet_number, $user_id, $star_date, $end_date){
-        file_put_contents('data.txt', print_r([$cabinet_number], 1));
-        /*$link = $this->connect();
-        $query = "INSERT INTO orders (firstname, lastname, email) VALUES ('John', 'Doe', 'john@example.com')";*/
     }
 }
